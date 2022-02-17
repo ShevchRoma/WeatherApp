5f350a706f038c723a5b82d4a8ec0f2e68262965
Roman Shevchenko <65279235+ShevchRoma@users.noreply.github.com>
2022-02-17 20:58:06 +0200

    Update addCityReducer.jsx

--- weather/src/store/reducers/addCityReducer.jsx
+++ weather/src/store/reducers/addCityReducer.jsx
@@ -2,14 +2,25 @@
 export const ADD_CITY = 'ADD_CITY';
 export const DELETE_CITY = 'DELETE_CITY';
 
+const initialState = {
+   cities: []
+}
 
    const addCity = (state = [],action) =>{
         switch(action.type){
             case ADD_CITY:
-                return [...state,action.payload]
+                return{
+               ...state,
+                   cities: [...state.cities,action.payload]
+                }
                 
             case DELETE_CITY:
-                 return state.filter(item => item.id !== action.payload.id)
+              return{
+                cities:[
+                    ...state.cities.slice(0,action.payload),
+                    ...state.cities.slice(action.payload + 1)
+                ]
+              }
                      
                   
                 default:
